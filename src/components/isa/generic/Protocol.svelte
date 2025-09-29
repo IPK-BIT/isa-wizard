3aebfff81acd68e99c8726470e74b67905ecf006
cheychus <cichacki@ipk-gatersleben.de>
2025-09-29 15:19:54 +0200

    Added basic implementation for protocol formular

--- src/components/isa/generic/Protocol.svelte
+++ src/components/isa/generic/Protocol.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
+  import ProtocolParameters from "./ProtocolParameters.svelte";
+  import String from "./String.svelte";
+  import Textarea from "./Textarea.svelte";
 
-    let {
-        attr = '',
-        componentConfig = {},
-        label = 'Protocol',
-        value: protocol = $bindable(),
-    } = $props();
+  let { attr = "", componentConfig = {}, label = "Protocol", value: protocol = $bindable() } = $props();
 
-    </script>
+let nameObj = {
+  label: "Name",
+  attr: '',
+  value: protocol[0].name,
+}
 
-    <pre>{JSON.stringify(protocol)}</pre>
+</script>
+
+<pre>{JSON.stringify(protocol)}</pre>
+
+<String label={"Name"} attr="name" bind:value={protocol[0].name} />
+
+<Textarea label={"Description"} attr="description" bind:value={protocol[0].description} />
+
+<String label={"URI"} attr="uri" bind:value={protocol[0].uri} />
+
+<ProtocolParameters bind:value={protocol.parameters} />
+
+<String label={"Version"} attr="" bind:value={protocol[0].version} />
+
+
+
+
+
+
---
+++ src/components/isa/generic/ProtocolParameters.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Schema from "@/lib/schemas";
+  import String from "./String.svelte";
+  import { onMount } from "svelte";
+
+  let { value = $bindable() } = $props();
+
+  function _getParameter(parameterName: string) {
+    let parameter = Schema.getObjectFromSchema("protocol_parameter");
+    parameter.parameterName = Schema.getObjectFromSchema("ontology_annotation");
+    parameter.parameterName.annotationValue = parameterName;
+
+    let commentForValue = Schema.getObjectFromSchema("comment");
+    commentForValue.name = "value";
+    commentForValue.value = "";
+    parameter.comments = [commentForValue];
+    return parameter;
+  }
+
+  console.log(_getParameter("Temperature"));
+
+
+</script>
+
+
+<String label={"Protocol Parameters"} attr="parameters" />
