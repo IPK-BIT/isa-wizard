052cbc7f0feacc7d2538ad07276fbbdd4cdbd9c0
Alexander Cichacki <a.cichacki@ostfalia.de>
2025-10-17 04:55:01 +0200

    removed unused import (OntologyAnnotation)

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import OntologyAnnotation from "./components/isa/generic/OntologyAnnotation.svelte";
-
     let config = $props();
 
     import Header from "./components/layout/Header.svelte";
@@ -16,48 +14,46 @@
     import { isaObj, isaStr } from "./stores/isa";
 
     let show = $state(true);
-
 </script>
 
 {#if getAppState() === AppState.Init}
-<InitView/>
+    <InitView />
 {:else}
-    {#if config.general.layoutMode === 'standalone'}
-    <Header/>
+    {#if config.general.layoutMode === "standalone"}
+        <Header />
     {/if}
 
-    <main class="content" class:grid={config.general.layoutMode === 'standalone'}>
-        {#if config.general.layoutMode === 'standalone'}
+    <main class="content" class:grid={config.general.layoutMode === "standalone"}>
+        {#if config.general.layoutMode === "standalone"}
             <div class="leftcol">
-            {#if getAppState() === AppState.Wizard && config.general.showQuestionnaireProgressBar}
-                <ProgressBar/>
-            {:else if getAppState() === AppState.Form}
-                <p>Tree</p>
-            {/if}
+                {#if getAppState() === AppState.Wizard && config.general.showQuestionnaireProgressBar}
+                    <ProgressBar />
+                {:else if getAppState() === AppState.Form}
+                    <p>Tree</p>
+                {/if}
             </div>
         {/if}
         <div class="middlecol">
             <div class="bbox">
                 {#if getAppState() === AppState.Form}
-                <p>Form View</p>
+                    <p>Form View</p>
                 {:else if getAppState() === AppState.Wizard}
-                <GenericQuestionnaire/>
+                    <GenericQuestionnaire />
                 {:else if getAppState() === AppState.Review}
-                <p>Review</p>
+                    <p>Review</p>
                 {/if}
             </div>
         </div>
 
-        {#if config.general.layoutMode === 'standalone' && config.general.showConsole}
-        <div class="rightcol">
-           
-            <div class="json bbox">
-                 <button onclick={() => show = !show}>Show ISA</button>
-                 {#if show}
-                <textarea bind:value={$isaStr}></textarea>
-                {/if}
-            </div>            
-        </div>
+        {#if config.general.layoutMode === "standalone" && config.general.showConsole}
+            <div class="rightcol">
+                <div class="json bbox">
+                    <button onclick={() => (show = !show)}>Show ISA</button>
+                    {#if show}
+                        <textarea bind:value={$isaStr}></textarea>
+                    {/if}
+                </div>
+            </div>
         {/if}
     </main>
 {/if}
@@ -70,7 +66,7 @@
     main.content.grid {
         display: grid;
         grid-template-columns: 1fr 3fr 1fr;
-        grid-template-rows: .25rem auto;
+        grid-template-rows: 0.25rem auto;
         grid-column-gap: 0px;
         grid-row-gap: 0px;
     }
@@ -95,7 +91,7 @@
         padding: 20px 0;
         padding-left: 10px;
         padding-right: 15px;
-        border-left: 0px solid rgb(190,190,190);
+        border-left: 0px solid rgb(190, 190, 190);
     }
 
     div.json {
@@ -109,10 +105,10 @@
         height: 100%;
         min-height: 500px;
         padding: 2px;
-        color: rgb(30,30,30);
+        color: rgb(30, 30, 30);
         overflow-x: scroll;
         white-space: pre;
-        border: 1px solid rgb(180,180,180);
+        border: 1px solid rgb(180, 180, 180);
         margin-top: 3px;
         font-size: 0.9em;
         font-family: monospace;
