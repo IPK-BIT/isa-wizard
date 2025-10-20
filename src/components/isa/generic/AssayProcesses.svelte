60462ccab0c3b41e22199e1275f4bdc56a66658a
cheychus <cichacki@ipk-gatersleben.de>
2025-10-20 17:19:34 +0200

    WIP: ImageProesses for study results

--- src/components/isa/generic/AssayProcesses.svelte
+++ src/components/isa/generic/AssayProcesses.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import ImageProcesses from "./ImageProcesses.svelte";
+
   interface Study {
     studyDbId: string; // The ID which uniquely identifies a study within the given database server - MIAPPE V1.1 (DM-11) Study unique ID - Unique identifier comprising the name or identifier for the institution/database hosting the submission of the study data, and the identifier of the study in that institution.
     studyName: string; // The human readable name for a study - MIAPPE V1.1 (DM-12) Study title - Human-readable text summarising the study
@@ -13,7 +15,7 @@
     imageFileSize?: string; // The size of the image in Bytes.
   }
 
-    let { componentConfig={} } = $props();
+  let { componentConfig = {}, value = $bindable() } = $props();
 
   let brapiURL = $derived(componentConfig.brapiBaseURL);
   let allStudies: Study[] = $state([]); // all available studies
@@ -23,10 +25,11 @@
   let loading: boolean = $state(false);
   const PAGE_SIZE = 8; // controls how many image cards are shown initially before Load More Button gets visible
   let page = 0;
+  let approve = $state(false);
 
   let errorLog = $state(""); // This is for showing errors which can occur when fetching data from BrAPI server
 
-
+  // $inspect(allImages);
 
   /**
    * Fetching data from the specified URL
@@ -38,7 +41,7 @@
 
     try {
       if (!brapiURL) {
-        throw new Error('Please specify a brapi url');
+        throw new Error("Please specify a brapi url");
       }
 
       const response = await fetch(brapiURL + parameter, {
@@ -96,93 +99,118 @@
   }
 </script>
 
-<div class="server-container">
-  <div>
-    <label>
-      Server:
-      <input
-        bind:value={brapiURL}
-        type="text"
-        class="input-url"
-        placeholder="URL of the BrAPI endpoint. Usually of the form /brapi/v2/"
-      />
-    </label>
-    <button
-      class="btn large"
-      onclick={async () =>
-        (allStudies = (await fetchBrapiData("studies")) ?? [])}>Load</button
-    >
-  </div>
-</div>
+{#if !approve}
+  <div class="grid padding">
+    <!-- First Row -->
+    <div class="flex-center padding">
+      <label for="server-input"> Server: </label>
+    </div>
+    <div class="server-container">
+      <input id="server-input" bind:value={brapiURL} type="text" class="input-url" placeholder="URL of the BrAPI endpoint. Usually of the form /brapi/v2/" />
+      <div class="flex-center">
+        <button class="btn large" style="min-width: 150px;" onclick={async () => (allStudies = (await fetchBrapiData("studies")) ?? [])}>Load</button>
+      </div>
+    </div>
 
-{#if allStudies.length > 0}
-  <p>{allStudies.length} Studies found</p>
-
-  <label>
-    Study:
-    <select
-      bind:value={selectedStudy}
-      class="study-select"
-      disabled={allStudies.length === 0}
-    >
-      {#each allStudies as study}
-        <option value={study.studyDbId} class="option"
-          >{study.studyDbId} - {study.studyName}</option
-        >
-      {/each}
-    </select>
-  </label>
-
-  <button
-    class="btn large"
-    onclick={async () => {
-      await loadImages();
-      // just for testing
-      // testMultipleImages(10);
-    }}>Load Images</button
-  >
-
-  <p>{allImages.length} Images found</p>
-
-  <div class="image-grid">
-    {#each visibleImages as img}
-      <div class="image-card">
-        <div class="image-meta">
-          <div class="filename">{img.imageFileName}</div>
-          <div class="description">{img.description}</div>
-          <a class="link" href={img.imageURL} target="_blank" rel="noopener">
-            {img.imageDbId}
-          </a>
+    <!-- Second Row -->
+    {#if allStudies.length > 0}
+      <div class="flex-center padding">
+        <label for="study-select"> Study: </label>
+      </div>
+      <div class="server-container">
+        <select id="study-select" bind:value={selectedStudy} class="study-select" style="flex: 1;" disabled={allStudies.length === 0}>
+          {#each allStudies as study}
+            <option value={study.studyDbId} class="option">{study.studyDbId} - {study.studyName}</option>
+          {/each}
+        </select>
+        <div class="flex-center">
+          <button
+            class="btn large"
+            style="min-width: 150px;"
+            onclick={async () => {
+              await loadImages();
+              // just for testing
+              // testMultipleImages(10);
+            }}>Find Images</button
+          >
         </div>
       </div>
-    {/each}
 
-    <div class="button-container">
-      {#if visibleImages.length < allImages.length}
-        <button class="btn btn-secondary" onclick={loadMore}>Load more</button>
-      {/if}
-    </div>
+      <!-- Third Row -->
+      <div class="padding">
+        <p>{allImages.length} Images found</p>
+      </div>
+      <div class="image-grid">
+        {#each visibleImages as img}
+          <div class="image-card">
+            <div class="image-meta">
+              <div class="filename">{img.imageFileName}</div>
+              <div class="description">{img.description}</div>
+              <a class="link" href={img.imageURL} target="_blank" rel="noopener">
+                {img.imageDbId}
+              </a>
+            </div>
+          </div>
+        {/each}
+
+        <div class="button-container">
+          {#if visibleImages.length < allImages.length}
+            <button class="btn btn-secondary" onclick={loadMore}>Load more</button>
+          {/if}
+        </div>
+      </div>
+    {/if}
   </div>
-{/if}
 
-{#if loading}
-  <p>loading...</p>
-{/if}
+  {#if allImages.length > 0}
+    <div class="btn-container padding">
+      <button disabled={false} class:disabled={false} class="btn btn-primary" onclick={() => (approve = true)}>Approve</button>
+    </div>
+  {/if}
 
-{#if errorLog}
-  <p style="color: red;">{errorLog}</p>
+  <div class="grid padding">
+    {#if loading}
+      <p class="padding">loading...</p>
+    {/if}
+
+    {#if errorLog}
+      <p class="padding" style="color: red;">{errorLog}</p>
+    {/if}
+  </div>
+{:else}
+  <ImageProcesses {value} {allImages} />
 {/if}
 
 <style>
+  /* .grid and .padding are used as a workaround to set the basic form layout from isa wizard  */
+  .grid {
+    display: grid;
+    grid-template-columns: 20% 80%;
+  }
+
+  .padding {
+    padding: 8px;
+  }
+
+  .flex-center {
+    display: flex;
+    align-items: center;
+  }
+
   .server-container {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    gap: 4px;
     padding: 16px 0px;
+  }
+
+  #server-input {
+    flex: 1;
   }
 
   .image-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 1rem;
     margin-top: 1rem;
   }
@@ -250,5 +278,20 @@
   .study-select {
     min-width: 300px;
     padding: 8px;
+  }
+
+  .btn-container {
+    display: flex;
+    justify-content: end;
+  }
+
+  .btn-container > .btn {
+    width: 150px;
+    height: 50px;
+  }
+
+  button.btn.disabled {
+    background: linear-gradient(0deg, hsl(0, 0%, 70%) 0%, hsl(0, 0%, 85%) 100%) !important;
+    color: black;
   }
 </style>
